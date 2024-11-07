40d52748c87c28e9401010f8288add00e5f76477
TecceLone <tnt728595@gmail.com>
2024-11-07 13:21:52 +0700

    Change

--- src/com/group19/effect/CacheDataLoader.java
+++ src/com/group19/effect/CacheDataLoader.java
@@ -1,5 +1,28 @@
 package com.group19.effect;
 
+import java.awt.Frame;
+import java.util.Hashtable;
+
 public class CacheDataLoader {
+
+    private static CacheDataLoader instance = null;
+
+    public static final int X_FRAME = 9;
+    public static final int Y_FRAME = 20;
+    public static final int WIDTH_FRAME = 130;
+    public static final int HEIGHT_FRAME = 115;
+
+    private String framefile = "data/frame.txt";
+
+    private Hashtable<String, FrameImage> frameImages;
+
+    private CacheDataLoader() {}
+
+    public static CacheDataLoader getInstance(){
+        if(instance == null){
+            instance = new CacheDataLoader();
+        }
+        return instance;
+    }
 
 }
--- src/com/group19/effect/FrameImage.java
+++ src/com/group19/effect/FrameImage.java
@@ -1,62 +1,46 @@
 package com.group19.effect;
 
-/*
-import java.awt.Graphics;
-import java.awt.Graphics2D; // ve co ban thi 2D hay ko thi cx giong nhau nma de 2D cu the hon con cai khong co 2D thi nhieu ham nhan lam tham so
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
-
 public class FrameImage {
-     private String name;
-	private BufferedImage image; // dung de luu tru hinh anh
 
-	public FrameImage(String name1, BufferedImage image1) {
-		this.name = name1;
-		this.image = image1;
-	}
-	
-	public FrameImage(FrameImage frameImage) {
-		
-		image = new BufferedImage(frameImage.getImageWidth(), frameImage.getImageHeight(),frameImage.getImage().getType());
-		Graphics g = image.getGraphics(); // graphics nhu kieu cọ vẽ của image
-		g.drawImage(frameImage.getImage(),0,0,null);
-		
-	}
-	
-	FrameImage(){
-		
-		image = null;
-		name = null;
-		
-	}
-	
-	public void draw(Graphics2D g2, int x, int y) {
-		g2.drawImage(image,x - image.getWidth()/2, y - image.getHeight()/2,null);
-	}
-	
-	public int getImageWidth() {
-		return image.getWidth();
-	}
-	
-	public int getImageHeight() {
-		return image.getHeight();
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name1) {
-		name = name1;
-	}
-	
-	public BufferedImage getImage() {
-		return image;
-	}
-	
-	public void setImage(BufferedImage image1) {
-		image = image1;
-	}
- */
+    private String name;
+    private BufferedImage image;
+
+    public FrameImage(String name, BufferedImage image) {
+        this.name = name;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    public int getWidthImage(){
+        return image.getWidth();
+    }
+
+    public int getHeightImage(){
+        return image.getHeight();
+    }
+
+    public void draw(int x, int y, Graphics2D g2){
+
+        g2.drawImage(image, x - image.getWidth()/2, y - image.getHeight()/2, null);
+
+    }
 
 }
