e4bb2bb90ddb2b6091c7f73f23cdfb740e594696
TecceLone <tnt728595@gmail.com>
2024-11-11 16:51:13 +0700

    Tạm thời xong phần Load Frame Image

--- src/com/group19/effect/CacheDataLoader.java
+++ src/com/group19/effect/CacheDataLoader.java
@@ -1,7 +1,13 @@
 package com.group19.effect;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.awt.Frame;
+import java.io.IOException;
 import java.util.Hashtable;
+import javax.imageio.ImageIO;
 
 public class CacheDataLoader {
 
@@ -23,6 +29,68 @@ public class CacheDataLoader {
             instance = new CacheDataLoader();
         }
         return instance;
+    }
+
+    public FrameImage getFrameImage(String name){
+
+        FrameImage frameImage = new FrameImage(instance.frameImages.get(name));
+        return frameImage;
+
+    }
+
+    public void LoadData() throws IOException{
+
+        LoadFrame();
+
+    }
+
+    public void LoadFrame() throws IOException{
+
+        frameImages = new Hashtable<String, FrameImage>();
+
+        FileReader fr = new FileReader(framefile);
+        BufferedReader br = new BufferedReader(fr);
+
+        String line = null;
+
+        if(br.readLine() == null){
+            System.out.println("No data");
+            throw new IOException();
+        }
+        else{
+
+            while((line = br.readLine()).equals(""));
+
+            int n = Integer.parseInt(line);
+            String path = null;
+            BufferedImage imageData = null;
+            String[] str = new String[70]; //Dùng để lưu tên và filepath
+            int currentStrIndex = 0;
+
+            for(int i=0; i<n; i++){
+
+                FrameImage frame = new FrameImage();
+                while((line = br.readLine()).equals(""));
+                str = line.split(" ");
+                frame.setName(str[currentStrIndex]);
+                currentStrIndex++;
+                path = str[currentStrIndex];
+                currentStrIndex++;
+
+                imageData = ImageIO.read(new File(path));
+                if(imageData != null){
+                    BufferedImage image = imageData.getSubimage(X_FRAME, Y_FRAME, WIDTH_FRAME, HEIGHT_FRAME);
+                    frame.setImage(image);
+                }
+
+                instance.frameImages.put(frame.getName(), frame);
+
+            }
+
+        }
+
+        br.close();
+
     }
 
 }
